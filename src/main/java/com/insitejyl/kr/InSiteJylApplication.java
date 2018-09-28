3ece8b6242174d648a5dff47dbc246b70eaa3242
JYL <ollehing@gmail.com>
2018-09-28 17:44:27 +0900

    test ver0.1

---
+++ src/main/java/com/insitejyl/kr/InSiteJylApplication.java
@@ -0,0 +1,13 @@
+package com.insitejyl.kr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InSiteJylApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(InSiteJylApplication.class, args);
+    }
+}
---
+++ src/main/java/com/insitejyl/kr/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.insitejyl.kr.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Locale;
+
+@Controller
+public class TestController {
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(Locale locale, Model model){
+
+        System.out.println("=============================================================================");
+        System.out.println("===============inSite Spring by Jae Yoon Lee - Get Test=======================");
+        System.out.println("=============================================================================");
+        System.out.println("================Home Page Requested, locale = " + locale + "=================");
+
+        return "test";
+    }
+
+}
